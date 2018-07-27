9e9e11d2ba8fdfff8b0597e3e2ddd78cfa5fc9da
Guillaume Manzano <>
2018-07-27 18:27:43 +0200

    add intro video

--- Quiz/Quiz/MenuViewController.swift
+++ Quiz/Quiz/MenuViewController.swift
@@ -19,9 +19,11 @@ class MenuViewController: UIViewController {
     @IBOutlet weak var scoreButton: UIButton!
     /// player button
     @IBOutlet weak var playButton: UIButton!
+    /// dark background for the video
+    @IBOutlet weak var viewForVideo: UIView!
     /// video player for the quiz video
     private var player: AVPlayer?
-
+    private var playerLayer: AVPlayerLayer?
     // - MARK: Methods
     /**
      Called after the controller's view is loaded into memory.
@@ -31,6 +33,7 @@ class MenuViewController: UIViewController {
         
         scoreButton.setShadow()
         playButton.setShadow()
+        self.loadVideo()
     }
     
     /**
@@ -46,34 +49,36 @@ class MenuViewController: UIViewController {
     */
     private func loadVideo(){
         // Load the video from the app bundle.
-        let videoURL: URL = Bundle.main.url(forResource: "quiz", withExtension: "mov")! as URL
+        let videoURL: URL = Bundle.main.url(forResource: "glitch", withExtension: "mp4")! as URL
         
         player = AVPlayer(url: videoURL)
         player?.actionAtItemEnd = .none
         player?.isMuted = true
         
-        let playerLayer = AVPlayerLayer(player: player)
-        playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
-        playerLayer.zPosition = 9
-        playerLayer.frame = CGRect(x: 0, y: 50, width: 300, height: 150)
+        self.playerLayer = AVPlayerLayer(player: player)
+            
+            playerLayer?.videoGravity = AVLayerVideoGravityResizeAspect
+            playerLayer?.frame = CGRect(x: 0 , y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
         
-        self.scoreButton.layer.zPosition = 1
-        self.playButton.layer.zPosition = 1
+        if let playerLayer = self.playerLayer {
+            view.layer.addSublayer(playerLayer)
+        }
         
-        view.layer.addSublayer(playerLayer)
-        
-        player?.play()
+            player?.play()
         
         //loop video
-        NotificationCenter.default.addObserver(self, selector: #selector(MenuViewController.loopVideo), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(MenuViewController.stopVideo), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
     }
     
     /**
      replay the video (loop)
     */
-    internal func loopVideo() {
-        player?.seek(to: kCMTimeZero)
-        player?.play()
+    internal func stopVideo() {
+        self.player?.pause()
+        self.player?.replaceCurrentItem(with: nil)
+        self.player = nil
+        self.playerLayer?.removeFromSuperlayer()
+        self.viewForVideo.removeFromSuperview()
     }
 }
 
